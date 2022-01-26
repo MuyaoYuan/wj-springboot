a0e8c20a8b3b0124c333a720c93043206195a0bc
yuanmuyao <yuanmuyao.stu@gmail.com>
2022-01-26 05:16:55 -0800

    实现图片上传

--- src/main/java/my/study/controller/LibraryController.java
+++ src/main/java/my/study/controller/LibraryController.java
@@ -2,9 +2,13 @@ package my.study.controller;
 
 import my.study.entity.Book;
 import my.study.service.BookService;
+import my.study.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -49,6 +53,25 @@ public class LibraryController {
             return bookService.list();
         } else {
             return bookService.Search(keywords);
+        }
+    }
+
+    @CrossOrigin
+    @PostMapping("api/covers")
+    public String coversUpload(MultipartFile file) throws Exception {
+        String folder = "/home/muyao/workspace/img";
+        File imageFolder = new File(folder);
+        File f = new File(imageFolder, StringUtils.getRandomString(6) + file.getOriginalFilename()
+                .substring(file.getOriginalFilename().length() - 4));
+        if (!f.getParentFile().exists())
+            f.getParentFile().mkdirs();
+        try {
+            file.transferTo(f);
+            String imgURL = "http://localhost:8443/api/file/" + f.getName();
+            return imgURL;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
         }
     }
 }
